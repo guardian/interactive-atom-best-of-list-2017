add52a295ac304095000264c1d096c64662b1145
cfen <chris.fenn@guardian.co.uk>
2017-11-17 15:46:17 +0000

    Converted colors to sass vars and optimised css gradients

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
 // javascript goes here
+
+function init(){
+	console.log(document.querySelector('.immersive-main-media__headline-container'));
+}
+
+
+//.immersive-main-media__headline-container--dark
+
+
+init();
