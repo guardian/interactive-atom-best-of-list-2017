b246a3cc3639458d63c6c3578ca371fef6b830a1
cfen <chris.fenn@guardian.co.uk>
2017-11-22 16:30:44 +0000

    albums cfg and css

--- deploy/1511366448239/boot.js
+++ deploy/1511366448239/boot.js
@@ -16,16 +16,16 @@ define([], function() {
             el.innerHTML = '<div style="font-size: 24px; text-align: center; padding: 72px 0; font-family: \'Guardian Egyptian Web\',Georgia,serif;">Loading…</div>';
 
             config = {
-                'assetPath': 'https://interactive.guim.co.uk/2017/11/review-list-enhancers/albums/1511368104273'
+                'assetPath': 'https://interactive.guim.co.uk/2017/11/review-list-enhancers/albums/1511366448239'
             };
 
             // Load CSS asynchronously
             window.setTimeout(function() {
-                addCSS('https://interactive.guim.co.uk/2017/11/review-list-enhancers/albums/1511368104273/main.css');
+                addCSS('https://interactive.guim.co.uk/2017/11/review-list-enhancers/albums/1511366448239/main.css');
             }, 10);
 
             // Load JS and init
-            require(['https://interactive.guim.co.uk/2017/11/review-list-enhancers/albums/1511368104273/main.js'], function(main) {
+            require(['https://interactive.guim.co.uk/2017/11/review-list-enhancers/albums/1511366448239/main.js'], function(main) {
                 main.init(el, context, config, mediator);
             }, function(err) { console.error('Error loading boot.', err); });
         }
